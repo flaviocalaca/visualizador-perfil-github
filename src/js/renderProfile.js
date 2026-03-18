ca4b05c9927dc6614da048fedb7d6f1d25652f7e
Jose Flavio <flaviopereiralc@gmail.com>
2026-03-17 21:08:38 -0300

    feat: adiciona verificação caso não tenha nome cadstrato

--- src/js/renderProfile.js
+++ src/js/renderProfile.js
@@ -39,7 +39,7 @@ export function renderProfile(userData, userRepos, container) {
       <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
 
       <div class="profile-info">
-        <h2>${userData.name}</h2>
+        <h2>${userData.name || "Nome não informado"}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
       </div>
     </div>
